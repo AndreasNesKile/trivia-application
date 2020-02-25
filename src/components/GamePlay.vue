47243411d182aec715e0349931bf3e463c601449
Andreas Nes Kile <andrenk@stud.ntnu.no>
2020-02-25 21:39:05 +0100

    Question and answers now showing

--- src/components/GamePlay.vue
+++ src/components/GamePlay.vue
@@ -1,12 +1,12 @@
 <template>
-    <v-container>
-        <!-- <v-col
-                v-for="(currentQuestion, index) in questions"
-                :key="currentQuestion + index"
-            > -->
-        <quiz-question :question="questions[0]" />
-        <!-- </v-col> -->
-    </v-container>
+  <v-container>
+    <quiz-question
+      v-if="questions.length > 0"
+      v-on:answered="nextQuestion"
+      :question="questions[currentQuestionIndex]"
+      @sendAnswer="handleAnswerFromUser"
+    />
+  </v-container>
 </template>
 
 <script>
@@ -16,27 +16,47 @@ import toastr from "toastr";
 import axios from "axios";
 
 export default {
-    name: "quiz-main-page",
-    components: {
-        "quiz-question": QuizQuestion
-    },
-    data() {
-        return {
-            questions: []
-        };
-    },
+  name: "quiz-main-page",
+  components: {
+    "quiz-question": QuizQuestion
+  },
+  data() {
+    return {
+      questions: [],
+      currentQuestionIndex: {
+        type: Number,
+        default: 0
+      },
+      score: 0
+    };
+  },
 
-    mounted() {
-        axios.get("https://opentdb.com/api.php?amount=20").then(response =>
-            (this.questions = response.data.results).catch(error => {
-                toastr.error(
-                    "ERROR",
-                    "Could not fetch questions from API",
-                    error
-                );
-            })
-        );
+  methods: {
+    handleAnswerFromUser: e => {
+      console.log("Parent:" + e);
+    },
+    randomIntFromInterval(min, max) {
+      // min and max included
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    },
+    nextQuestion() {
+      this.currentQuestionIndex++;
     }
+  },
+
+  mounted() {
+    let numberOfQuestions = this.randomIntFromInterval(10, 20);
+
+    axios
+      .get(`https://opentdb.com/api.php?amount=${numberOfQuestions}`)
+      .then(response => {
+        this.questions = response.data.results;
+      })
+      .catch(error => {
+        toastr.error("ERROR", "Could not fetch questions from API", error);
+      });
+    this.currentQuestionIndex = 0;
+  }
 };
 </script>
 
--- src/components/QuizQuestion.vue
+++ src/components/QuizQuestion.vue
@@ -1,36 +1,56 @@
 <template>
-    <v-card v-if="question.type === 'multiple'">
-        <v-card-title justify-center>{{ question.question }}</v-card-title>
-        <v-card-text v-for="answer in answers" :key="answer">
-            <v-row>
-                <v-col>
-                    <v-sheet> {{ answer }}</v-sheet>
-                </v-col>
-            </v-row>
-        </v-card-text>
-    </v-card>
+  <v-card>
+    <v-card-title justify-center>{{ question.question }}</v-card-title>
+    <v-card-text>
+      <v-row>
+        <v-col v-for="(answer,index) in answers" :key="index" xl="6" md="6" l="6" sm="12" xs="12">
+          <v-card class="quiz-card-container elevation-3" @click="sendAnswer">
+            <v-card-text>{{ answer }}</v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
 export default {
-    props: ["question"],
-    data() {
-        return {
-            answers: []
-        };
-    },
+  props: {
+    question: {}
+  },
+  data() {
+    return {
+      answers: []
+    };
+  },
 
-    watch: {
-        question: (newVal, oldVal) => {
-            this.answers.push(newVal["correct_answer"]);
-            newVal["incorrect_answers"].forEach(answer => {
-                this.answers.push(answer);
-            });
-            this.debouncedGetAnswer();
-            console.log(oldVal);
-        }
+  methods: {
+    sendAnswer() {
+      this.$emit("answered");
     }
+  },
+
+  mounted() {
+    this.answers = [
+      ...this.question.incorrect_answers,
+      this.question.correct_answer
+    ];
+  },
+
+  watch: {
+    question: () => {
+      this.answers = [
+        ...this.question.incorrect_answers,
+        this.question.correct_answer
+      ];
+    }
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.quiz-card-container:hover {
+  background-color: #d3d3d3;
+  cursor: pointer;
+}
+</style>
