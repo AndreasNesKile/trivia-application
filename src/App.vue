8eaf0b3e91503691a871481e2e55f9b00f3de8a2
Andreas Nes Kile <andreasnk95@gmail.com>
2020-02-25 10:17:40 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="app">
+        <nav>
+            <ul>
+                <li>
+                    <router-link to="/home">Home</router-link>
+                </li>
+            </ul>
+        </nav>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+    name: "App",
+    components: {
+        // HelloWorld
+    }
+};
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
